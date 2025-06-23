445aa9a572770c8438ed8826324396f7b9635722
Sushant <sus.hant@outlook.com>
2025-06-23 12:37:56 +1000

    uses static states instead of extracting from the stations

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Container from "@/components/container"
 import MapView from "@/components/map-view";
 import http from "@/plugins/http";
 import { useEffect, useState } from "react";
+import australianStates from "@/store/australian-states.js"
 
 export default function Home() {
   const [stations, setStations] = useState([]);
@@ -20,11 +21,10 @@ export default function Home() {
   }, [state, stations])
 
   useEffect(() => {
-    const uniqueStates = Array.from(new Set(stations.map(s => s.state)));
-    if(uniqueStates.length){
-      setStates(['All', ...uniqueStates])
-    }
-  }, [stations])
+    const states = [...australianStates];
+    states.unshift({code: "All", title: "Please select a state."})
+      setStates(states)
+  }, [])
 
   useEffect(() => {
     async function getStations(){
@@ -45,15 +45,16 @@ export default function Home() {
       <h1 className="text-2xl text-center my-4">Welcome to Weather Station!</h1>
       <div className="flex justify-between gap-4">
           {states?.length > 0 && (
-            <div className="w-3xs">
-              <label for="state">Select State:</label>
+            <div className="w-3xs border p-3 rounded border-gray-200 drop-shadow-sm">
+              <h2 className="text-lg mb-4">Filter:</h2>
+              <label>State:</label>
               <select className="w-full border border-gray-300 rounded p-1"
                 value={state}
                 onChange={(e) => setState(e.target.value)}
               >
                 {states.map(state => (
-                  <option value={state} key={state}>
-                    {state}
+                  <option value={state.code} key={state.code}>
+                    {state.title}
                   </option>
                 ))}
               </select>
---
+++ src/store/australian-states.js
@@ -0,0 +1,12 @@
+const states = Object.freeze([
+  { code: "NSW", title: "New South Wales" },
+  { code: "VIC", title: "Victoria" },
+  { code: "QLD", title: "Queensland" },
+  { code: "WA",  title: "Western Australia" },
+  { code: "SA",  title: "South Australia" },
+  { code: "TAS", title: "Tasmania" },
+  { code: "ACT", title: "Australian Capital Territory" },
+  { code: "NT",  title: "Northern Territory" }
+]);
+
+export default states;
